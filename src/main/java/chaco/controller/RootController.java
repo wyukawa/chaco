1aba38aab89fa4f31cc4b0ade7606f7291be3beb
wyukawa <wyukawa@gmail.com>
2015-10-23 17:15:10 +0900

    Add query execution logic

--- src/main/java/chaco/controller/RootController.java
+++ src/main/java/chaco/controller/RootController.java
@@ -1,7 +1,12 @@
 package chaco.controller;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import javax.inject.Inject;
@@ -9,27 +14,50 @@ import javax.inject.Inject;
 import freemarker.template.TemplateException;
 import lombok.extern.slf4j.Slf4j;
 import me.geso.avans.annotation.GET;
+import me.geso.avans.annotation.POST;
 import me.geso.avans.annotation.Param;
+//import chaco.service.NetezzaService;
 import chaco.service.MySQLService;
 import me.geso.tinyorm.TinyORM;
 import me.geso.webscrew.response.WebResponse;
 
 @Slf4j
 public class RootController extends BaseController {
-	@Inject
-	private TinyORM db;
-	@Inject
-	private MySQLService mySQLService;
+    @Inject
+    private TinyORM db;
+    @Inject
+    private MySQLService mySQLService;
+    //private NetezzaService netezzaService;
 
-	@GET("/")
-	public WebResponse index(@Param("name") Optional<String> name) throws IOException, TemplateException, SQLException {
-		final String databaseProductName = db.getConnection()
-			.getMetaData().getDatabaseProductName();
-		return this.freemarker("index.html.ftl")
-			.param("name", name.orElse("Anonymous"))
-			.param("db", databaseProductName)
-			.param("jdbcMajorVersion", mySQLService.getJDBCMajorVersion())
-			.render();
-	}
+    @GET("/")
+    public WebResponse index() throws IOException, TemplateException, SQLException {
+        return this.freemarker("index.html.ftl")
+                .param("rows", new ArrayList<>())
+                .render();
+    }
+
+    @POST("/query")
+    public WebResponse select(@Param("sql") Optional<String> sql) throws IOException, TemplateException, SQLException {
+        try (Connection connection = db.getConnection()) {
+            try (PreparedStatement stmt = connection.prepareStatement(sql.orElse(""))) {
+                try (ResultSet resultSet = stmt.executeQuery()) {
+                    List<List<String>> rows = new ArrayList<>();
+                    int columnCount = stmt.getMetaData().getColumnCount();
+                    while (resultSet.next()) {
+                        List<String> columns = new ArrayList<>();
+                        for (int i = 1; i <= columnCount; i++) {
+                            String column = resultSet.getString(i);
+                            columns.add(column);
+                        }
+                        rows.add(columns);
+                    }
+
+                    return this.freemarker("index.html.ftl")
+                            .param("rows", rows)
+                            .render();
+                }
+            }
+        }
+    }
 }
 
--- src/main/webapp/WEB-INF/templates/index.html.ftl
+++ src/main/webapp/WEB-INF/templates/index.html.ftl
@@ -3,12 +3,24 @@
 <#-- @ftlvariable name="name" type="java.lang.String" -->
 <#include "include/header.html.ftl">
 
-<div class="row">
-    Hello, world!
-    <p>${name}</p>
-    <p>${db}</p>
+<form action="/query" method="post">
+    <div class="form-group">
+        <label for="exampleInputEmail1">SQL</label>
+        <textarea class="form-control" rows="3" name="sql"></textarea>
+    </div>
+    <button type="submit" class="btn btn-default">Execute</button>
+</form>
 
-    <p>${jdbcMajorVersion}</p>
-</div>
+<table class="table table-bordered">
+    <thead>
+        <#list rows as row>
+            <tr>
+            <#list row as column>
+                <td>${column}</td>
+            </#list>
+            </tr>
+        </#list>
+    </thead>
+</table>
 
 <#include "include/footer.html.ftl">
