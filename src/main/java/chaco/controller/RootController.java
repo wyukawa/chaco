41b05e388497cff855f77b4a51c482305d7aa0d3
wyukawa <wyukawa@gmail.com>
2015-10-30 17:35:07 +0900

    Add validation

--- src/main/java/chaco/controller/RootController.java
+++ src/main/java/chaco/controller/RootController.java
@@ -14,15 +14,13 @@ import lombok.extern.slf4j.Slf4j;
 import me.geso.avans.annotation.GET;
 import me.geso.avans.annotation.POST;
 import me.geso.avans.annotation.Param;
-import me.geso.tinyorm.TinyORM;
 import me.geso.webscrew.response.WebResponse;
 
 import chaco.service.NetezzaService;
 
 @Slf4j
 public class RootController extends BaseController {
-    @Inject
-    private TinyORM db;
+
     @Inject
     private NetezzaService netezzaService;
 
@@ -32,7 +30,6 @@ public class RootController extends BaseController {
         return this.freemarker("index.html.ftl")
                 .param("query", "")
                 .param("rows", new ArrayList<>())
-                .param("tableNames", tableNames)
                 .param("error", "")
                 .render();
     }
@@ -66,8 +63,15 @@ public class RootController extends BaseController {
     }
 
     @GET("/tableNames")
-    public WebResponse getSchemaNames(@Param("catalog") Optional<String> catalogOptional, @Param("schema") Optional<String> schemaOptinal) {
+    public WebResponse getTableNames(@Param("catalog") Optional<String> catalogOptional, @Param("schema") Optional<String> schemaOptinal) {
+
         HashMap<String, Object> retVal = new HashMap<String, Object>();
+
+        if (!catalogOptional.isPresent() || !schemaOptinal.isPresent()) {
+            retVal.put("error", "catalog and schema parameters are required");
+            return this.renderJSON(retVal);
+        }
+
         List<String> tableNames = null;
         try {
             tableNames = netezzaService.getTableNames(catalogOptional.orElse(""), schemaOptinal.orElse(""));
@@ -80,39 +84,29 @@ public class RootController extends BaseController {
     }
 
     @POST("/query")
-    public WebResponse select(@Param("query") Optional<String> queryOptional) throws IOException, TemplateException {
-        List<String> tableNames = null;
+    public WebResponse query(@Param("query") Optional<String> queryOptional) throws IOException, TemplateException {
+
+        HashMap<String, Object> retVal = new HashMap<String, Object>();
+
+        if (!queryOptional.isPresent()) {
+            retVal.put("error", "query is required");
+            return this.renderJSON(retVal);
+        }
+
+        List<List<String>> rows = new ArrayList<>();
+        String error = "";
         try {
-            tableNames = netezzaService.getTableNames("test", "test_schema");
+            rows = netezzaService.getResultRows(queryOptional.get());
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            log.error(e.getMessage(), e);
+            error = e.getMessage();
         }
-        if (queryOptional.isPresent()) {
-            List<List<String>> rows = new ArrayList<>();
-            String error = "";
-            try {
-                rows = netezzaService.getResultRows(queryOptional.get());
-            } catch (SQLException e) {
-                log.error(e.getMessage(), e);
-                error = e.getMessage();
-            }
-            return this.freemarker("index.html.ftl")
-                    .param("query", queryOptional.orElse(""))
-                    .param("rows", rows)
-                    .param("tableNames", tableNames)
-                    .param("error", error)
-                    .render();
-        } else {
-            return this.freemarker("index.html.ftl")
-                    .param("query", "")
-                    .param("rows", new ArrayList<>())
-                    .param("tableNames", tableNames)
-                    .param("error", "")
-                    .render();
-        }
-
+        return this.freemarker("index.html.ftl")
+                .param("query", queryOptional.orElse(""))
+                .param("rows", rows)
+                .param("error", error)
+                .render();
     }
-
 
 }
 
