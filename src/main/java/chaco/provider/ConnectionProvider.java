f0838dddbdb44fee2087e1a18707265e573c8236
wyukawa <wyukawa@gmail.com>
2015-11-10 20:07:43 +0900

    Add Class.forName

--- src/main/java/chaco/provider/ConnectionProvider.java
+++ src/main/java/chaco/provider/ConnectionProvider.java
@@ -27,6 +27,12 @@ public class ConnectionProvider implements Provider<Connection> {
 			if (connection == null) {
 				log.debug("Creating new JDBC connection: {}", config.getJdbc().getUrl());
 
+				try {
+					Class.forName(config.getJdbc().getDriver());
+				} catch (ClassNotFoundException e) {
+					throw new RuntimeException(e);
+				}
+
 				connection = DriverManager.getConnection(
 					config.getJdbc().getUrl(),
 					config.getJdbc().getUsername(),
