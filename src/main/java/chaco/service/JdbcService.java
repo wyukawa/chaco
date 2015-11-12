66f26ef7f855661c3df6835176a6cf7044d62aa9
wyukawa <wyukawa@gmail.com>
2015-11-12 14:16:08 +0900

    Add ResultSetMetaData

--- src/main/java/chaco/service/JdbcService.java
+++ src/main/java/chaco/service/JdbcService.java
@@ -8,6 +8,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.ResultSetMetaData;
 import java.util.ArrayList;
 import java.util.List;
 
